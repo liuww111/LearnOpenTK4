f6ab331751c07a76734a20b26e4fc4cba6dc729a
fourone <702295399@qq.com>
2023-04-03 16:03:54 +0800

    扩展增加TexturePlus

--- CreateWord5/Window.cs
+++ CreateWord5/Window.cs
@@ -20,23 +20,12 @@ namespace LearnOpenTK
     /// </summary>
     public class Window : GameWindow
     {
-        private readonly uint[] _indices =
-        {
-            0, 1, 3,
-            1, 2, 3
-        };
-
-        private int _elementBufferObject;
-
-        private int _vertexBufferObject;
-
-        private int _vertexArrayObject;
 
         private Shader _shader;
 
-        private Texture _texture1;
+        private TexturePlus _texture1;
 
-        private Texture _texture2;
+        private TexturePlus _texture2;
 
         private System.Threading.Timer _computeTimer;
 
@@ -83,11 +72,32 @@ namespace LearnOpenTK
             //FPS(e);
 
             var key = base.KeyboardState;
-
+            int step = 10;
             if (key.IsKeyDown(Keys.Escape))
             {
                 Close();
             }
+            if (key.IsKeyDown(Keys.W))
+            {
+                _texture1.MovePosition(_texture1.X, _texture1.Y + step);
+                _texture2.MovePosition(_texture2.X, _texture2.Y + step);
+            }
+            if (key.IsKeyDown(Keys.S))
+            {
+                _texture1.MovePosition(_texture1.X, _texture1.Y - step);
+                _texture2.MovePosition(_texture2.X, _texture2.Y - step);
+            }
+            if (key.IsKeyDown(Keys.A))
+            {
+                _texture1.MovePosition(_texture1.X - step, _texture1.Y);
+                _texture2.MovePosition(_texture2.X - step, _texture2.Y);
+            }
+            if (key.IsKeyDown(Keys.D))
+            {
+                _texture1.MovePosition(_texture1.X + step, _texture1.Y);
+                _texture2.MovePosition(_texture2.X + step, _texture2.Y);
+            }
+
         }
          
         /// <summary>
@@ -99,22 +109,20 @@ namespace LearnOpenTK
             base.OnRenderFrame(e);
             //开始用设定的颜色来清空屏幕
             GL.Clear(ClearBufferMask.ColorBufferBit);
-            GL.BindVertexArray(_vertexArrayObject);
+            //GL.BindVertexArray(_vertexArrayObject);
 
-            Debug.WriteLine($"mixVal:{minVal % 1} ---- {(int)(minVal % 4)}");
+            //Debug.WriteLine($"mixVal:{minVal % 1} ---- {(int)(minVal % 4)}");
             _shader.SetFloat("mixVal", Convert.ToSingle(minVal % 1));
             //_shader.SetFloat("mixVal", 0.5f);
-            _texture2 = Texture.LoadFromFile($"Resources/Img/bag{(int)(minVal % 2)}.png");
-
-            _texture1.Use(TextureUnit.Texture0);
-            _texture2.Use(TextureUnit.Texture1);
+            //_texture2 = TexturePlus.LoadFromFile($"Resources/Img/bag{(int)(minVal % 2)}.png");
             _shader.Use();
 
-            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            _texture1.Use(unit: TextureUnit.Texture0);
+            _texture2.Use(unit: TextureUnit.Texture1);
 
             base.SwapBuffers();//交换缓冲，建议最后
         }
-
+        
         protected override void OnUnload() {
             base.OnUnload();
         }
@@ -126,48 +134,22 @@ namespace LearnOpenTK
         }
 
         private void MixChangeInit() {
-            //float xpos = 0;
-            //float ypos = 0;
-            float w = Size.X;
-            float h = Size.Y;
-
-            float[] vertices = {
-                w-144,  h-44,  1.0f, 1.0f, // top right
-                w-144, 0.0f, 1.0f, 0.0f, // bottom right
-                0.0f, 0.0f, 0.0f, 0.0f, // bottom left
-                0.0f,  h-44, 0.0f, 1.0f  // top left
-            };
-
-            _vertexArrayObject = GL.GenVertexArray();
-            GL.BindVertexArray(_vertexArrayObject);
-
-            _vertexBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
-
-            _elementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+             
 
             _shader = new Shader("Shaders/mixShader.vert", "Shaders/mixShader.frag");
-
             // 指定采样器的取的纹理单元
             _shader.SetInt("texture0", 0);
             _shader.SetInt("texture1", 1);
-
             _shader.SetMatrix4("projection", Shader.GLMOrthographic(0.0f, Size.X, 0.0f, Size.Y));//投影的场景大小
-
-            GL.EnableVertexAttribArray(0);
-            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 4 * sizeof(float), 0);
-
-            _texture1 = Texture.LoadFromFile("Resources/Img/bag2.png");
-            _texture2 = Texture.LoadFromFile("Resources/Img/bag3.png");
+ 
+            _texture1 = TexturePlus.LoadFromFile("Resources/Img/bag0.png");
+            _texture2 = TexturePlus.LoadFromFile("Resources/Img/bag2.png");
 
             //每0.1秒进行一次变换
             _computeTimer = new System.Threading.Timer(t =>
             {
                 minVal = minVal + change;
-            }, null, 0, 100);
+            }, null, 0, 200);
         }
     }
 }
